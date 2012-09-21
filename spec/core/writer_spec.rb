7b9249d91e530559175522d54344c972f7fa1a82
Xavier Via <xavierviacanel@gmail.com>
2012-09-21 16:25:59 -0300

    Writes #to

--- spec/core/writer_spec.rb
+++ spec/core/writer_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe Core::Writer do
-  describe "#from" do
+  describe "#to" do
     context "when the methos is call" do
       before do
         module Writer
@@ -18,13 +18,13 @@ describe Core::Writer do
         Writer::TestClass.should_receive :new
 
         obj = TestClass.new
-        obj.from
+        obj.to
       end
 
       it "should pass as argument self" do
         obj = TestClass.new
         Writer::TestClass.should_receive(:new).with(obj)
-        obj.from
+        obj.to
       end
 
     end
